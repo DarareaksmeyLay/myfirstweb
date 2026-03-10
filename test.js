5cadd3fb4e3cf86f20a1b5badc3d7e00551dc4db
William Lay <darareaksmeylay@gmail.com>
2026-03-10 09:50:48 +0000

    restore file

--- test.js
+++ test.js
@@ -1,12 +1,4 @@
 const arr = ["reaksmey","vannarath","lytith"];
-// console.log(arr.length);
-// arr.unshift("hello")
-arr.shift();
-arr.push("hello");
-
-
-
-
 console.log(arr);
 console.table(arr);
-console.log(arr.unshift())
+console.log(arr.length);
