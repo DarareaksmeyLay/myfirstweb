dfedd6da8ae7d464d930280ef5ccae9a54c9b26c
William Lay <darareaksmeylay@gmail.com>
2026-03-10 09:13:20 +0000

    delete one file

--- test.js
+++ test.js
@@ -1,4 +1,12 @@
 const arr = ["reaksmey","vannarath","lytith"];
+// console.log(arr.length);
+// arr.unshift("hello")
+arr.shift();
+arr.push("hello");
+
+
+
+
 console.log(arr);
 console.table(arr);
-console.log(arr.length);
+console.log(arr.unshift())
