af98ecf5897e486077147c1699a1402343853022
DarareaksmeyLay <darareaksmeylay@gmail.com>
2026-03-11 00:43:38 +0700

    last update

--- test.js
+++ test.js
@@ -4,13 +4,13 @@ arr.push("sokly");
 
 arr.shift();
 
-const newArr = [2000,2001,2002];
+// const newArr = [2000,2001,2002];
 
-const combinedArr = arr.concat(newArr);
+// const combinedArr = arr.concat(newArr);
 
 console.log(arr);
 console.table(arr);
 console.log(arr.length);
 
-console.log(combinedArr.reverse());
-console.log(combinedArr.length)
+// console.log(combinedArr.reverse());
+// console.log(combinedArr.length)
