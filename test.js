73af274b24fb0d902ce4b04ad23d8fb8fe5d11a9
DarareaksmeyLay <darareaksmeylay@gmail.com>
2026-03-11 00:30:26 +0700

    Update for test.js

--- test.js
+++ test.js
@@ -1,4 +1,16 @@
 const arr = ["reaksmey","vannarath","lytith"];
+
+arr.push("sokly");
+
+arr.shift();
+
+const newArr = [2000,2001,2002];
+
+const combinedArr = arr.concat(newArr);
+
 console.log(arr);
 console.table(arr);
 console.log(arr.length);
+
+console.log(combinedArr.reverse());
+console.log(combinedArr.length)
